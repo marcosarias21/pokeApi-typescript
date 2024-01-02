77a41abe1ebf93561c08bd43d8b2bfd990f80b98
marcosarias21 <marcosariasolaiz@gmail.com>
2024-01-02 15:12:45 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../vite.svg'
 import './App.css'
 
 const App = () => {
@@ -10,7 +9,7 @@ const App = () => {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -18,7 +17,7 @@ const App = () => {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
+        <button onClick={() => { setCount(count + 1) }}>
           count is {count}
         </button>
         <p>
